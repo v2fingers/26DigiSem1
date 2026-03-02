5c9027a910a5b16a6c61b4bf1da1f915e2b25c4f
v2fingers <24087bs@kuranuicollege.school.nz>
2026-03-03 12:29:29 +1300

    Added Calculator App

---
+++ Calculator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace NewConsoleApp
+
+{
+    
+    class Program 
+    {
+        static void Main() 
+        {
+            while (true)
+            {
+                Calculator myCalculator = new Calculator();
+                myCalculator.Calculate();
+                bool again = myCalculator.Again();
+
+                if(again)
+                {
+                    myCalculator.Calculate();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    interface ICalculator
+    {
+        void Calculate();
+        bool Again();
+    }
+
+    class Calculator : ICalculator
+    {
+        public bool Again()
+        {
+            bool again;
+            Console.WriteLine("Another Calculation? y/n");
+            string myString = Console.ReadLine();
+            if (myString == "y")
+            {
+                again = true;
+            }
+            else
+            {
+                again = false;
+            }
+
+            return again;
+        }
+        
+        public void Calculate()
+        {
+            string operation;
+            Console.WriteLine("Enter First Number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter Operation Type: ");
+            operation = Console.ReadLine();
+            
+            Console.WriteLine("Enter Second Number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (operation == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (operation == "/")
+            {
+                if (num1 != 0 && num2 != 0)
+                {
+                    Console.WriteLine(num1 / num2);
+                }
+                else
+                {
+                    Console.WriteLine("Division by zero is not allowed.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation.");
+            }
+            
+        }
+    }
+}
